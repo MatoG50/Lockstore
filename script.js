e5e0a868fd1bdd6603eca79d3e6dffdaf4fa57d8
MatoG50 <matog50@gmail.com>
2021-05-04 18:15:03 +0300

    Added a click event and edited button

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const loginButton = document.querySelector('.login-btn');
+const loginEmail = document.querySelector('#email');
+const loginPassword = document.querySelector('#password');
+
+let loginDetails = {};
+
+loginButton.addEventListener('click', function () {
+  loginDetails.email = loginEmail.value;
+  loginDetails.password = loginPassword.value;
+});
+
+console.log(loginDetails);
