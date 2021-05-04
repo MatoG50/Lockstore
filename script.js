10a3bff5f2d9b65bf84e9a388bb83e34f6784a2d
Paulstar200 <vitalispaul48@live.com>
2021-05-04 20:34:16 +0300

    Add fetch

--- script.js
+++ script.js
@@ -6,9 +6,35 @@ const loginPassword = document.querySelector('#password');
 
 let loginDetails = {};
 
+let loginUrl = "https://storemanagerapi2.herokuapp.com/api/v2/auth/login"
+
 loginButton.addEventListener('click', function () {
   loginDetails.email = loginEmail.value;
   loginDetails.password = loginPassword.value;
 
   console.log(loginDetails);
+
+  fetch(loginUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(loginDetails),
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
 });
+
+
+
+// const loginUser = () => {
+//   loginDetails.email = loginEmail.value;
+//   loginDetails.password = loginPassword.value;
+
+//   console.log(loginDetails);
+// }
