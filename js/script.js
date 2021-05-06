87bde68386d6a010317ceabf24b65cc7ab4806bd
MatoG50 <matog50@gmail.com>
2021-05-06 14:38:41 +0300

    Added loader

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const loginEmail = document.querySelector('#email');
 const loginPassword = document.querySelector('#password');
 const loginButton = document.getElementById('login-btn');
 const errorMessage = document.getElementById('error-message');
+const loader = document.querySelector('.loader');
 
 let loginDetails = {};
 let loginUrl = 'https://storemanagerapi2.herokuapp.com/api/v2/auth/login';
@@ -11,7 +12,8 @@ let loginUrl = 'https://storemanagerapi2.herokuapp.com/api/v2/auth/login';
 const loginUser = function () {
   loginDetails.email = loginEmail.value;
   loginDetails.password = loginPassword.value;
-  loginButton.innerHTML = 'LOGGING IN...';
+  loginButton.style.display = 'none';
+  loader.style.display = 'block';
 
   console.log(loginDetails);
 
@@ -35,15 +37,26 @@ const loginUser = function () {
       ) {
         errorMessage.innerHTML = data.message;
         errorMessage.style.display = 'block';
+        loginButton.style.display = 'block';
+        loader.style.display = 'none';
       } else if (data.message === 'Log in successful!') {
         errorMessage.style.display = 'none';
         localStorage.setItem('token', data.access_token);
+        loginButton.style.display = 'block';
+        loader.style.display = 'none';
       } else {
         errorMessage.style.display = 'none';
+        loginButton.style.display = 'block';
+        loader.style.display = 'none';
       }
     })
     .catch(error => {
       console.error('Error:', error);
-      loginButton.innerHTML = 'LOG IN';
+      loginButton.style.display = 'block';
+      loader.style.display = 'none';
     });
+};
+
+const hideError = function () {
+  errorMessage.style.display = 'none';
 };
