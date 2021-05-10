1ce303a90d9fdfc78808d35e42104cff832b63ec
MatoG50 <matog50@gmail.com>
2021-05-10 15:41:15 +0300

    Made Webpage responsive

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,16 +1,22 @@
 'use strict';
 const userName = document.querySelector('.user-name');
 const userRole = document.querySelector('.user-role');
+const mobiUserName = document.querySelector('.mobi-user-name');
+const mobiUserRole = document.querySelector('.mobi-user-role');
 const firstName = document.querySelector('.first-name');
 const totalProducts = document.getElementById('total-products');
 const totalemployees = document.getElementById('total-employees');
+const salesPerformance = document.getElementById('sales-performance');
 const todaysDate = new Date().toDateString();
 const date = document.querySelector('.date');
 const productsUrl = 'https://storemanagerapi2.herokuapp.com/api/v2/products';
 const usersUrl = 'https://storemanagerapi2.herokuapp.com/api/v2/users';
+const salesUrl = 'https://storemanagerapi2.herokuapp.com/api/v2/sales';
 
 userName.innerHTML = localStorage.getItem('username');
 userRole.innerHTML = localStorage.getItem('role');
+mobiUserName.innerHTML = localStorage.getItem('username');
+mobiUserRole.innerHTML = localStorage.getItem('role');
 
 firstName.innerHTML = localStorage.getItem('username');
 date.innerHTML = todaysDate;
@@ -52,6 +58,21 @@ if (!token) {
     .catch(error => {
       console.error('Error:', error);
     });
+  fetch(salesUrl, {
+    method: 'GET', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+    },
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+      salesPerformance.innerHTML = `${data.Sales.length}%`;
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 
   if (localStorage.getItem('role') === 'attendant') {
     document.querySelector('.photo').innerHTML =
@@ -59,6 +80,13 @@ if (!token) {
   } else {
     document.querySelector('.photo').innerHTML =
       '<img src="../resources/paul.jfif" id="user-photo" alt="Admin" />';
+  }
+  if (localStorage.getItem('role') === 'attendant') {
+    document.querySelector('.mobi-photo').innerHTML =
+      '<img src="../resources/MGK.jpg" id="mobi-photo" alt="Admin" />';
+  } else {
+    document.querySelector('.mobi-photo').innerHTML =
+      '<img src="../resources/paul.jfif" id="mobi-photo" alt="Admin" />';
   }
 }
 
@@ -68,3 +96,13 @@ const logout = () => {
   localStorage.removeItem('username');
   window.location.href = '../UI/index.html';
 };
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+  document.getElementById('mySidepanel').style.width = '250px';
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+  document.getElementById('mySidepanel').style.width = '0';
+}
