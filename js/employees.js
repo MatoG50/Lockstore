1c5a497b51b87459d5cf51523acd238d3082efb7
MatoG50 <matog50@gmail.com>
2021-05-12 16:03:29 +0300

    Connected employees to backend

--- js/employees.js
+++ js/employees.js
@@ -5,6 +5,45 @@
 const token = localStorage.getItem('token');
 console.log('Token:', token);
 
+// Employees
+const employeesUrl = 'https://storemanagerapi2.herokuapp.com/api/v2/users';
+const totalEmployees = document.querySelector('.total-employees');
+const attendants = document.querySelector('.att-span');
+const admins = document.querySelector('.admin-span');
+const employeesContainer = document.querySelector('.employee-details');
+
+fetch(employeesUrl, {
+  method: 'GET', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
+})
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    totalEmployees.innerHTML = data.users.length;
+    const attendantTotals = data.users.filter(cur => cur.role === 'attendant');
+    attendants.innerHTML = attendantTotals.length;
+    const adminTotals = data.users.filter(cur => cur.role === 'admin');
+    admins.innerHTML = adminTotals.length;
+    data.users.forEach(function (usr) {
+      const userPhoto =
+        usr.username === 'PaulVitalis'
+          ? '<img src="../resources/paul.jfif" alt="user-image" id="user-image">'
+          : '<img src="../resources/user.svg" alt="user-image" id="user-image">';
+      const html = ` <div class="employee-card">
+      ${userPhoto}
+      <p class="employee-card-name">${usr.username}</p>
+      <P class="employee-card-role">${usr.role}</P>
+  </div>`;
+      employeesContainer.insertAdjacentHTML('beforeend', html);
+    });
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
 // User
 const userName = document.querySelector('.user-name');
 const userRole = document.querySelector('.user-role');
@@ -40,6 +79,7 @@ const employees = () => {
 const dashboard = () => {
   window.location.href = '../UI/dashboard.html';
 };
+document.querySelector('.emp-col').style.color = '#0984e3';
 const addEmployee = () => {
   window.location.href = '../UI/add-employee.html';
 };
